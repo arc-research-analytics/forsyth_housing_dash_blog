b8f6af3245709c3c42c15d86b955e26823905447
Will Wright <willwright@Wills-MacBook-Pro-2.local>
2023-05-23 13:17:11 -0400

    updated plotly

--- dash_blog.py
+++ dash_blog.py
@@ -406,7 +406,7 @@ def charter():
             dtick = 'M3',
             range=['2021-1','2023-4']
             ),
-        height=390,
+        height=425,
         hovermode="closest")
 
     # add shifting vertical lines
